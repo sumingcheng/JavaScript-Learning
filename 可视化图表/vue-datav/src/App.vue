39a4c779a4223f287a1bdc21b3855ed6c170fead
cjwdyzdyplxjg <fanxiaohaohaha@qq.com>
2022-01-06 22:09:31 +0800

    每个图表一个组件

--- 可视化图表/vue-datav/src/App.vue
+++ 可视化图表/vue-datav/src/App.vue
@@ -12,5 +12,20 @@
 </script>
 
 <style>
+  div {
+    box-sizing: border-box;
+  }
 
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+
+  #app{
+    min-width: 1290px;
+    height: 100%;
+  }
 </style>
