a37b383bb3f111b16739ad5cf351460db5fa600b
cjwdyzdyplxjg <fanxiaohaohaha@qq.com>
2022-01-06 22:42:24 +0800

    组件创建

---
+++ 可视化图表/vue-datav/src/components/TotaIview/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="total-view">
+    <TodayUsers></TodayUsers>
+    <TotalOrders></TotalOrders>
+    <TotalSales></TotalSales>
+    <TotalUsers></TotalUsers>
+  </div>
+
+</template>
+
+<script>
+  import TodayUsers from './Sub/TodayUsers'
+  import TotalOrders from './Sub/TotalOrders'
+  import TotalSales from './Sub/TotalSales'
+  import TotalUsers from './Sub/TotalUsers'
+
+
+  export default {
+    components: {
+      TodayUsers,
+      TotalOrders,
+      TotalSales,
+      TotalUsers,
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- 可视化图表/vue-datav/src/views/Home.vue
+++ 可视化图表/vue-datav/src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="home">
-
+    <TotalView></TotalView>
   </div>
 </template>
 
 <script>
-  export default{
-    mounted(){
-      console.log('home',this.$echarts);
+  import TotalView from '../components/TotaIview/index.vue'
+  export default {
+    components: {
+      TotalView
     }
+
   }
 </script>
+
+<style>
+.home{
+  width: 100%;
+  height: 100%;
+}
+</style>
