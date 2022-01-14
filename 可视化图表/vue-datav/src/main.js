ca0318131f2db4540acae29f748d8c0675f428b5
cjwdyzdyplxjg <fanxiaohaohaha@qq.com>
2022-01-14 23:09:34 +0800

    2022年1月14日 23:09:32

--- 可视化图表/vue-datav/src/main.js
+++ 可视化图表/vue-datav/src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue'
 import router from './router'
 import * as echarts from 'echarts';
 import './plugins/element.js'
+import VueECharts from 'vue-echarts'
 
 Vue.prototype.$echarts = echarts;
 
 Vue.config.productionTip = false
-
+Vue.component("v-chart", VueECharts);
 new Vue({
   router,
   render: h => h(App)
---
+++ 可视化图表/vue-datav/src/views/BMap.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bmap">
+    <v-chart :options="options"></v-chart>
+  </div>
+</template>
+
+<script>
+  import "echarts/extension/bmap/bmap"
+  export default {
+    data() {
+      return {
+        options: {}
+      }
+    },
+    mounted() {
+      this.options = {
+        bmap: {
+          key: 'Xo20QUK0DQzpZOzhGBlUi1GKd9Eys9dn',
+          center: [116.404, 39.915],
+          zoom: 6
+        }
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
