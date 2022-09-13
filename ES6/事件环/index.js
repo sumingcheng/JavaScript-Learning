6ce8df07934d6ef530324e442b6d877bdc36af89
Sumingcheng <fanxiaohaohaha@qq..com>
2022-09-14 07:59:12 +0800

    2022年9月14日 07:59:09

--- ES6/事件环/index.js
+++ ES6/事件环/index.js
@@ -13,18 +13,79 @@
 //
 // console.log(4);
 
-Promise.resolve().then(() => {
-  console.log('p1');
-  setTimeout(() => {
-    console.log('s2')
-  }, 0)
-})
+// Promise.resolve().then(() => {
+//   console.log('p1');
+//   setTimeout(() => {
+//     console.log('s2')
+//   }, 0)
+// })
+//
+// setTimeout(() => {
+//   console.log('s1');
+//   Promise.resolve().then(() => {
+//     console.log('p2');
+//   })
+// })
 
-setTimeout(() => {
-  console.log('s1');
-  Promise.resolve().then(() => {
-    console.log('p2');
+// console.log(1);
+//
+// setTimeout(() => {
+//   console.log(2)
+// }, 10);
+//
+// new Promise(function (resolve, reject) {
+//   // executor 执行器 同步代码
+//   console.log(3);
+//   // resolve('');
+//   console.log(4);
+// }).then(res => {
+//   console.log(5);
+// })
+// console.log(6);
+//
+// //1 3 4 6 5 2
+
+
+//  3 12 5 67 4
+// let res = function () {
+//   console.log(1)
+//   return new Promise((resolve, reject) => {
+//     console.log(2)
+//     resolve(4);
+//   })
+// }
+//
+// new Promise(async (resolve, reject) => {
+//   console.log(3);
+//   let test = await res();
+//   console.log(test);
+// })
+//
+// console.log(5)
+//
+// new Promise((resolve, reject) => {
+//   console.log(6)
+// })
+//
+// console.log(7)
+
+// 1 2 m1 m2
+const oBtn = document.getElementById('btn');
+
+oBtn.addEventListener('click', () => {
+  console.log(1);
+
+  Promise.resolve('m1').then((str) => {
+    console.log(str);
   })
-})
+}, false)
 
+oBtn.addEventListener('click', () => {
+  console.log(2);
 
+  Promise.resolve('m2').then((str) => {
+    console.log(str);
+  })
+}, false);
+
+oBtn.click();
