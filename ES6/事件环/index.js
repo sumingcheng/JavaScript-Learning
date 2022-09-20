34f4ec029ddbcff5a9641ece6aa28f38d0996221
Sumingcheng <fanxiaohaohaha@qq..com>
2022-09-20 21:28:35 +0800

    2022年9月20日 21:28:33

--- ES6/事件环/index.js
+++ ES6/事件环/index.js
@@ -243,4 +243,33 @@
 //   console.log(e.data);
 // };
 
+// const element = document.getElementById('box');
+// let start, previousTimeStamp;
+// let done = false;
+//
+// function step(timestamp) {
+//   if (start === undefined) {
+//     start = timestamp;
+//   }
+//   const elapsed = timestamp - start;
+//
+//   if (previousTimeStamp !== timestamp) {
+//     // 这里使用 `Math.min()` 确保元素刚好停在 200px 的位置。
+//     const count = Math.min(0.1 * elapsed, 200);
+//     element.style.transform = 'translateX(' + count + 'px)';
+//     if (count === 200) done = true;
+//   }
+//
+//   if (elapsed < 2000) { // 在两秒后停止动画
+//     previousTimeStamp = timestamp;
+//     if (!done) {
+//       window.requestAnimationFrame(step);
+//     }
+//   }
+// }
+//
+// window.requestAnimationFrame(step);
+
+
+
 
