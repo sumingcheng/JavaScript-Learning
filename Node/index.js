4e54164555c094e4332e6cf8024c8e9720a4db4d
Sumingcheng <fanxiaohaohaha@qq..com>
2022-09-29 13:08:26 +0800

    Node 10 以上的版本

--- Node/index.js
+++ Node/index.js
@@ -66,30 +66,32 @@
 // let aa = new F();
 // console.log(aa);
 
-function myTypeof(params) {
-  const type = Object.prototype.toString.call(params).slice(8, - 1).toLowerCase();
-  const map = {
-    'number': true,
-    'string': true,
-    'boolean': true,
-    'undefined': true,
-    'bigint': true,
-    'symbol': true,
-    'function': true
-  };
-  return map[type] ? type : 'object';
-}
+// function myTypeof(params) {
+//   const type = Object.prototype.toString.call(params).slice(8, - 1).toLowerCase();
+//   const map = {
+//     'number': true,
+//     'string': true,
+//     'boolean': true,
+//     'undefined': true,
+//     'bigint': true,
+//     'symbol': true,
+//     'function': true
+//   };
+//   return map[type] ? type : 'object';
+// }
+//
+// console.log(myTypeof(null));
+// // 测试用例
+// // myTypeof(1)
+// // myTypeof('')
+// // myTypeof(false)
+// // myTypeof(null)
+// // myTypeof(undefined)
+// // myTypeof(10n) // bigint
+// // myTypeof(Symbol())
+// // myTypeof(() => {})
 
-console.log(myTypeof(null));
-// 测试用例
-// myTypeof(1)
-// myTypeof('')
-// myTypeof(false)
-// myTypeof(null)
-// myTypeof(undefined)
-// myTypeof(10n) // bigint
-// myTypeof(Symbol())
-// myTypeof(() => {})
-myTypeof([]);
-myTypeof({});
+
+
+
 
---
+++ Node/node事件环.js
@@ -0,0 +1,33 @@
+// Promise.resolve().then(res => {
+//   console.log(1);
+// });
+// process.nextTick(() => {
+//   console.log(2);
+// });
+// console.log('start');
+//
+// readFile('1.txt', 'utf-8', () => {
+//   setTimeout(() => {
+//     console.log(3);
+//   });
+//
+//   process.nextTick(() => {
+//     console.log(4);
+//   });
+//
+//   setTimeout(() => {
+//     console.log(5);
+//   });
+// });
+//
+// console.log(6);
+//
+// setTimeout(() => {
+//   console.log(7);
+// });
+//
+// setImmediate(() => {
+//   console.log(8);
+// });
+//
+// console.log('end');
