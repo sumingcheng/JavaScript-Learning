941ce2353b23a0c5826e31219e00942d914a0a7a
fanxiaohao <fanxiaohao@mail.taiji.com.cn>
2022-12-19 16:43:34 +0800

    2022年12月19日 16:43:32

--- Tools/Tools.js
+++ Tools/Tools.js
@@ -82,3 +82,24 @@ function formatDateTime(time) {
     date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
   return year + "-" + month + "-" + day;
 }
+
+// Ver: 1.0
+// Last Update: 2022/08/25
+// 功能性修改请及时更新版本号并修改注释
+// setTimout 在计时结束后将任务加入队列，等待空闲资源执行
+// 计算上一次计时结束后的误差时间作为下一次的参数
+// 每次执行触发回调方法，剩余次数作为参数
+
+export function countdown(times, timeout, fun) {
+  if (times === 0) {
+    return
+  }
+  const startTime = new Date().getTime()
+  setTimeout(() => {
+    const endTime = new Date().getTime()
+    const deviation = endTime - startTime - timeout
+    times--
+    fun(times)
+    countdown(times, timeout - deviation, fun)
+  }, timeout)
+}
