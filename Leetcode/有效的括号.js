6c247d9a9b38522118c40f238161813dc41172af
fanxiaohao <fanxiaohao@mail.taiji.com.cn>
2023-02-20 11:25:59 +0800

    一上午没了

---
+++ Leetcode/有效的括号.js
@@ -0,0 +1,29 @@
+let s = "{{(())}}";
+var isValid = function (s) {
+  // 字符串为空
+  if (!s.length) return false;
+  // 字符串出现单独的情况
+  if (s.length % 2 !== 0) return false;
+
+  const pairs = new Map();
+  pairs.set('{', '}');
+  pairs.set('[', ']');
+  pairs.set('(', ')');
+
+  let stack = [];
+
+  for (let c of s) {
+    // 判断key是否存在
+    if (pairs.has(c)) {
+      // 存在就push到栈内
+      stack.push(c);
+      // 若当前项的key对应val，则符合括号的要求，并且继续进行出栈操作。如果不符合，直接false
+    } else if (pairs.get(stack.pop()) !== c) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
+
+console.log(isValid(s));
